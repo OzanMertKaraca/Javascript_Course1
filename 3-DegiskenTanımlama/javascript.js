d90eaa994b75d4254ef54c67aa4febdf2912fc64
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-17 21:28:23 +0300

    Değişken tanımlayıp örneklendirdim.

---
+++ 3-DegiskenTanımlama/javascript.js
@@ -0,0 +1,22 @@
+let gun=14;
+let ay=7;
+let yil=1999;
+
+console.log(gun,ay,yil);
+console.log("X Caffe "+gun+"/"+ay+"/"+yil+" "+"tarihinde açılmıştır.");
+
+gun=29;
+console.log(gun);
+
+const  sa=250000;
+console.log(sa);
+
+var a=10;
+a=20;
+console.log(a);
+
+let gn,ayi,yili;
+gn=5,ayi=10,yili=1999;
+console.log(gn,ayi,yili);
+
+console.log("abcd "+gn+"/"+ayi+"/"+yili+" abcd")
