da849f6b43b8e896005e5a6899276f96ff572748
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-18 21:57:03 +0300

    Numbers

---
+++ 6-Numbers/javascript.js
@@ -0,0 +1,37 @@
+let yaricap=15;
+const pisayisi=3.14;
+console.log(yaricap,pisayisi);
+
+let dairealani=pisayisi*yaricap**2;
+console.log(dairealani);
+
+console.log(16/4);
+
+let kalan=yaricap %4;
+console.log(kalan);
+
+let sonuc = 6*(15-4)**2;
+console.log(sonuc);
+
+let sayi = 13;
+sayi = sayi + 1 ;
+sayi +=1;
+sayi++;
+console.log(sayi);
+
+let sayi1=10;
+sayi1 *=3;
+console.log(sayi1);
+
+let sayi2=10;
+sayi2 /=2;
+console.log(sayi2);
+
+//NaN - Not a Number toplamada hata alınmayacaktır
+console.log(15/"AAA");
+console.log(10*"BBB");
+console.log(10+"CCC");
+
+
+
+
