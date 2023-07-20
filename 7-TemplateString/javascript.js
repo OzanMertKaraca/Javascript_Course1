8f28299a66c1678e69b21c866c21e5e76248a438
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-20 21:01:59 +0300

    Temlate Strings Kullanımı

---
+++ 7-TemplateString/javascript.js
@@ -0,0 +1,19 @@
+const framework1='Angular';
+const framework2='React';
+const framework3='Vue';
+
+//let sonuc='Günümüzde En Popüler Javascript Kütüphaneleridir.';
+//console.log(framework1+' , ' +framework2+' , '+framework3+' '+sonuc);
+
+let sonuc=framework1+' , ' +framework2+' , '+framework3+' '+'Günümüzde En Popüler Javascript Kütüphaneleridir.';
+console.log(sonuc);
+
+let sonuc1=`Günümüzde En Popüler Javascript Kütüphaneleridir. ${framework1} ${framework2} ${framework3}`
+console.log(sonuc1);
+
+let html= `
+<h1>${framework1}</h1>
+<h1>${framework2}</h1>
+<h1>${framework3}</h1>
+`
+console.log(html);
