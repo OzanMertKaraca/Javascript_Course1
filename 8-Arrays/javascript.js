1417449e8bf97e5ca26306903906ebaaa9b11cb4
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-22 17:12:23 +0300

    Arrays

---
+++ 8-Arrays/javascript.js
@@ -0,0 +1,36 @@
+let  isimler = ['Ozii','Barış'];
+//console.log(isimler[1]);
+
+//isimler[1]='Semiramis';
+//console.log(isimler);
+
+// alert("2+2"+isimler);
+
+let yaslar=[10,21,25,30];
+//console.log(yaslar[3]);
+
+let rastgele=['react','angular',20]
+//console.log(rastgele);
+
+// console.log(rastgele.length);
+
+let  tre=isimler.join('-');
+console.log(tre);
+
+let  virgl=isimler.join(',');
+console.log(virgl);
+
+let sira=isimler.indexOf('Barış');
+console.log(sira);
+
+let ekle = isimler.concat(['Karaca','Akarsu']);
+console.log(ekle);
+
+let elemanekle=isimler.push('Sevgi') ;
+console.log(isimler);
+
+let elemançıkar=isimler.pop();
+console.log(isimler);
+
+
+
