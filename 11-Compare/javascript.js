03d8d37bd9b57c74a16dfcb433088e6e856f1aae
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-24 22:26:59 +0300

    Compare

---
+++ 11-Compare/javascript.js
@@ -0,0 +1,45 @@
+// let yas=20;
+
+// console.log(yas ==20);
+// console.log(yas == "20");
+
+// console.log (yas === 20);  //3 eşit ile veri tipine göre de karşılaştırma yapılır.
+// console.log (yas === "20");
+
+// console.log(yas != 20);
+// console.log(yas != "20");
+
+// console.log(yas !== 20);
+// console.log(yas !== "20");
+
+//let yas ="20";
+// console.log(yas+1);
+// console.log(typeof yas);
+
+// yas = Number(yas);
+// console.log(yas+1);
+// console.log(typeof yas);
+
+// let sonuc=Number('Barış');
+// console.log(sonuc,typeof sonuc);
+
+// let sonuc1=String(20);
+// console.log(sonuc1,typeof sonuc1);
+
+let sonuc = Boolean(20); // 0 Haricinde sayılar True döndürür.
+console.log(sonuc , typeof sonuc);
+
+let sonuc2 = Boolean(0); // 0 Haricinde sayılar True döndürür.
+console.log(sonuc2 , typeof sonuc2);
+
+let sonuc3 = Boolean("0");
+console.log(sonuc3 , typeof sonuc3);
+
+let sonuc4 = Boolean(""); //Boş Değer olmadığı sürece False döndürmez.
+console.log(sonuc4 , typeof sonuc4);
+
+
+
+
+
+
