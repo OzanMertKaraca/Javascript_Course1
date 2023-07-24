225e901fa1ff578372cf73f8e29e77d9aa4eaaed
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-24 21:22:57 +0300

    Boolean Veri Tipi

---
+++ 10-Boolean/javascript.js
@@ -0,0 +1,25 @@
+// console.log(true,false);
+
+// let email='@gmail.com';
+// let adSoyad=['Ozan','Barış'];
+
+// let varmi=email.includes('@');
+// console.log(varmi);
+
+// let varmi1=adSoyad.includes('Ozan');
+// console.log(varmi1);
+
+// let yas=20;
+// console.log(yas == 20); //eşitmidir
+// console.log(yas==21);
+// console.log(yas !=21 ); // eşit değil mi 
+// console.log(yas > 21);
+// console.log(yas < 21);
+// console.log(yas >=20);
+// console.log(yas <=19);
+
+let ad='ozii';
+console.log(ad == 'ozii');
+console.log(ad == 'Ozii');
+console.log(ad > 'Ozii');
+console.log(ad < 'aaa'); 
