c5b7fce83470741eeb5d6eb7190d5674560647df
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-31 21:56:49 +0300

    Global-Local Değişken

---
+++ 19-Global-Local/javascript.js
@@ -0,0 +1,20 @@
+let yas = 29 ;
+
+
+
+if (true)
+{
+   let yas = 60 ;
+   console.log("İçerideki:" , yas);
+
+   if(true)
+   { 
+      let yas=55;
+      var ad="Barış";
+      let name="Ozan";
+      console.log("İçeridekinin İçindeki:", yas,ad,name);
+   }
+
+}
+
+console.log("Dışarıdaki:" , yas,name,ad);
