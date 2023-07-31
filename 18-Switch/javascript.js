620eb69581cfa9d5a91e76d30264f90e092addbf
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-07-31 21:38:26 +0300

    Switch

---
+++ 18-Switch/javascript.js
@@ -0,0 +1,17 @@
+const not = 'AA';
+
+switch(not)
+{
+   case 'AA':
+   console.log("Bravo");
+   break;
+   case 'B':
+   console.log("iyi");
+   break;
+   case 'C':
+   console.log("Geçer");
+   break;
+   default:
+   console.log("Kaldı");
+   break;
+}
