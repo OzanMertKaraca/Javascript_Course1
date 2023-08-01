5b8c65b66c01cf82eebdb9e6e7cf441aa6555f4d
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-08-01 21:30:00 +0300

    function-1.1

---
+++ 21-Fonsiyona Parametre Verme/javascript.js
@@ -0,0 +1,8 @@
+const ozan = function(ad='Ozan',soyad='Karaca') //Default Değer olarak veri girişi yaptım.
+{
+   // console.log("Ad: "+ ad +" Soyad: "+soyad);
+   console.log(`Ad-Soyad: ${ad} ${soyad}`);
+}
+
+ozan();//Parametre olarak ver igirişi yapılmadığında fonksiyon çağrıldığında default değeri yazdıracktır.
+ozan('Barış','Ahmet');
