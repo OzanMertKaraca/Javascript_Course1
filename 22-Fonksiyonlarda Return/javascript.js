58ec8321d733602cedd4c86c18e243d87f555739
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-08-01 21:48:14 +0300

    Function Return

---
+++ 22-Fonksiyonlarda Return/javascript.js
@@ -0,0 +1,28 @@
+const karealan = function(kenar)
+{
+    let alan=kenar**2;
+   console.log(alan);
+
+}
+
+karealan(6);
+
+
+const karealan1 = function(kenar1)
+{
+    let alan1=kenar1**2;
+    return alan1;
+}
+const sonuc1 = karealan1(6);
+
+console.log(sonuc1);
+
+const karealan2 = function(kenar2)
+{
+   return kenar2**2;
+}
+const sonuc2 = karealan2(6);
+
+console.log(sonuc2);
+
+
