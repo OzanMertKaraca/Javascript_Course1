ff5dbbd0afff7af48a6dc782ef57dc9f51b7b498
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-08-10 21:59:38 +0300

    t

---
+++ 33-GetElementById-ClassName-TagName/javascript.js
@@ -0,0 +1,11 @@
+//GetElementById
+//GetElementsByClassName
+//GetElementsTagName
+const baslik = document.getElementById('ozi');
+console.log(baslik);
+
+const hatalar = document.getElementsByClassName('error');
+console.log(hatalar[0]);
+
+const petiketleri = document.getElementsByTagName('p');
+console.log(petiketleri[1]);
