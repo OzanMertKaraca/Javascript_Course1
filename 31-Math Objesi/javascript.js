28a3acc25160f6b0ad8c468216e992acc24fec46
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-08-10 20:22:21 +0300

    Math Objesi

---
+++ 31-Math Objesi/javascript.js
@@ -0,0 +1,13 @@
+console.log(Math);
+console.log(Math.PI);
+
+let sayi = 5.3;
+console.log(Math.round(sayi)); //Virgülden sonra sayiyi En yakınına yuvarlar.
+console.log(Math.floor(sayi)); //Virgülden sonra sayiyi tabana yuvarlar.
+console.log(Math.ceil(sayi));  //Sayiyi tavana yuvarlar.
+console.log(Math.trunc(sayi));  //Noktadan Sonrasını siler.
+
+const rastgele = Math.random(); // Random değer üretir.
+
+console.log(rastgele);
+console.log(Math.ceil(rastgele*100));
