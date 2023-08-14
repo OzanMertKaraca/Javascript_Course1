adedf766ee439cf8d6dd5c1f21e17909ec521b60
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-08-14 21:08:32 +0300

    Var olan Css değiştirme

---
+++ 36-Css'e Müdahele Etme/javascript.js
@@ -0,0 +1,8 @@
+const renk = document.querySelector('h1');
+//renk.setAttribute('style','padding:50px;'); //Bu şekilde  kullanıldığında var olan özellikleri silmektedir.
+
+console.log(renk.style);
+
+renk.style.padding='50px';
+renk.style.fontSize='90px';
+renk.style.fontWeight='bolder';
