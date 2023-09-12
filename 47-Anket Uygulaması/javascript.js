3dc906175009b4b33d5d836f77384c34834a7762
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-12 21:27:38 +0300

    AnketUygulaması1.4

--- 47-Anket Uygulaması/javascript.js
+++ 47-Anket Uygulaması/javascript.js
@@ -17,7 +17,41 @@ form.addEventListener('submit', e => {
       
     // console.log(baspuan);
     result.classList.remove('d-none');//kaldırmak istediğim classı yazdım
+     
+    let puan = 0;
+    const yazdir = setInterval(() => {
+     
+      result.querySelector('span').textContent = `${puan}%`;//result classı içerisdeki span etiketine puanı yazdırdım
+      if(puan == baspuan)
+      {
+        clearInterval(yazdir);
+      }
+      else
+      {
+        puan++;
+      }
+
+    },15);
 
     result.querySelector('span').textContent = `${baspuan}%`;//result classı içerisdeki span etiketine puanı yazdırdım
 
     })
+
+    // setTimeout(() => { //setTimeout da içerisine yazılan kod 1 kere çalışır
+    //       console.log('ozii');
+    // },2000); // Saniye sonra yazırılır.
+
+    //  setInterval(() => {  //setInterval da içerisine yazılan kod hep çalışır
+    // console.log('ozii');
+    //   }, 2000); Her iki saniyede bir console ozii yazdırmaya devam eder.
+
+// let i = 0 ;
+// const yazdir = setInterval(() => {
+//     console.log('Ozii');
+//     i++;
+//     if(i == 5)
+//     {
+//         clearInterval(yazdir);
+//     } 
+// },1000); //setInterval in sürekli  çalışmasını önlemek amacıyla bu şekilde kullandım.
+
