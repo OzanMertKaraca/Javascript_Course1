2372f828168311271ea64cfcadea6516b30f547f
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-18 20:53:23 +0300

    Push and Pop

--- 52-Find Metodu/javascript.js
+++ 52-Find Metodu/javascript.js
@@ -1,7 +1,19 @@
-const points = [80 , 75 , 20 , 30 , 90 ];
-//-içerisindeki koşul true olduğunda diğer değerlere bakmadan sonucu döner.
+// Find Metodu
+// const points = [80 , 75 , 20 , 30 , 90 ];
+// //-içerisindeki koşul true olduğunda diğer değerlere bakmadan sonucu döner.
 
-const highpoint=points.find(point => {
-      return point > 70 
-});
-console.log(highpoint);
+// const highpoint=points.find(point => {
+//       return point > 70 
+// });
+// console.log(highpoint);
+
+//Push ve Pop Metodu
+const points = [80 , 75 , 20 , 30 , 90 ];
+points.push(100);//poinst dizisine yeni eleman ekledim.
+console.log(points);
+
+points.pop();
+console.log(points);
+
+points.pop();
+console.log(points);
