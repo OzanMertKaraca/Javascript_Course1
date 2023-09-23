d5395af33cd514acea689ccc842c9ce8f7432bdd
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-23 17:30:45 +0300

    TodoList.0.2ListeyeVeriEkl

---
+++ 59-Yapılacak Listesi Uyg/javascript.js
@@ -0,0 +1,28 @@
+const addform = document.querySelector('.add');
+const list =  document.querySelector('.todos');
+
+const listadd = (todo) => { //todo =>
+
+    const html = 
+    `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="far fa-trash-alt delete"></i>
+         </li>
+    `;
+    list.innerHTML+=html;
+};
+
+addform.addEventListener('submit',e => {
+    e.preventDefault();//Sayfa yenilemesini engellemek için kullanıyorum.
+    const todo=addform.add.value.trim();//Name add olan classın verisini çektim. trim() baştanve sondan boşluk kaldırır. sadece karakteri alır.
+    // console.log(todo);
+    if(todo.length)
+    {
+        listadd(todo)//Formdan aldığım değeri gönderdim.
+        addform.reset();//Veri eklenildikten sonra silme text temizleme
+    }
+    
+    
+})
+
