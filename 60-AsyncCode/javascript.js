2af19aaedc510f3463650652c8e775f006662251
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-25 20:22:00 +0300

    AsyncCode

---
+++ 60-AsyncCode/javascript.js
@@ -0,0 +1,11 @@
+console.log(1);
+console.log(2);
+
+setTimeout(() => {
+    console.log('ozii');
+},2000);
+//Sayfa yüklendikten    2   sn sonra yazdırılacak.
+console.log(3);
+console.log(4);
+console.log(5);
+
