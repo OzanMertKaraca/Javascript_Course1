74b3ea851e129a827978fe7dc3260e1fbf45e8a9
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-25 21:25:33 +0300

    callback function

---
+++ 63-Callback Function/javascript.js
@@ -0,0 +1,35 @@
+const gettodos = (callback) => {
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange',()=> {
+   // console.log(request,request.readyState);
+    if(request.readyState === 4 && request.status === 200)
+    {
+        // console.log(request.responseText);
+        callback(undefined,request.responseText);
+    }
+    
+    else if(request.readyState === 4)
+    {
+       //console.log("Başarısız");
+       callback('Başarısız',undefined);
+    }
+   
+});
+
+request.open('GET','https://jsonplaceholder.typicode.com/todoss');
+request.send();
+}
+
+gettodos((err,data) =>{
+   // console.log(err,data);
+   if(err)
+   {
+    console.log(err);
+   }
+   else
+   {
+    console.log(data);
+   }
+});
