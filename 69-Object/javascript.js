4cfcdcda40892115b03be19769836b93092cc2aa
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-09-28 17:57:54 +0300

    Object

---
+++ 69-Object/javascript.js
@@ -0,0 +1,15 @@
+const userOne={
+    username:'Ozan',
+    email:'@gmail.com',
+    login(){
+        console.log('Ozii Giriş Yaptı');
+    },
+    logout(){
+        console.log('Ozii Çıkış Yaptı');
+    },
+
+};
+
+console.log(userOne.email,userOne.username);
+userOne.login();
+
