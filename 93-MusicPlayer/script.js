ab5608bd873588142e2087364913e6db0f22a525
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-10-28 11:54:31 +0300

    tkmOyunfinish

---
+++ 93-MusicPlayer/script.js
@@ -0,0 +1,18 @@
+const music = document.querySelector('audio');
+const prevbutton = document.getElementById('prev');
+const playbutton = document.getElementById('play');
+const nextbutton = document.getElementById('next');
+
+// music.play();
+// music.pause();
+
+playbutton.addEventListener('click' , () => {
+  
+})
+
+function playsong(){
+    music.play();
+}
+function pausesong(){
+    music.pause();
+}
