ea734b065cc5471b3f0845329b596bee5bf9099e
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-10-31 20:25:50 +0300

    KayıtUygulaması1.1

---
+++ 94-KayıtGüncelleSilUyg/script.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('menu');
+const mainpopup = document.getElementById('mainpopup');
+const contentdiv = document.getElementById('contentdiv');
+
+menu.addEventListener('click' , popupload);
+
+function popupload(){
+    mainpopup.style.display='block';
+   
+}
