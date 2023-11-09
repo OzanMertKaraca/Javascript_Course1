165dba3ec241cf4ccd5f3a8ee36380387a1ff2fb
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-11-09 21:35:55 +0300

    MusicPlayerFinish

--- 93-MusicPlayer/script.js
+++ 93-MusicPlayer/script.js
@@ -5,6 +5,10 @@ const nextbutton = document.getElementById('next');
 const image = document.querySelector('img');
 const title = document.getElementById('title');
 const creater = document.getElementById('creater');
+const progressDiv = document.getElementById('progressDiv');
+const progress = document.getElementById('progress');
+const currenttimespan = document.getElementById('currenttime');
+const totaltimespan = document.getElementById('totaltime');
 
 let isplaying = false;
 
@@ -46,9 +50,46 @@ function pausesong(){
     music.pause();
 }
 
-prevbutton.addEventListener('click', prevsong);
+function updateprogressbar(e){
+    if(isplaying)
+    { 
+        const {currentTime,duration} = e.srcElement;
+        const progresspercent = (currentTime / duration) * 100;
+        progress.style.width= `${progresspercent}%`;
 
-nextbutton.addEventListener('click', nextsong);
+        const durationminutes = Math.floor(duration / 60);
+        let durationseconds = Math.floor(duration % 60);
+        if(durationseconds < 10){
+            durationseconds = `0${durationseconds}`;
+        }
+        if(durationseconds){
+            totaltimespan.textContent = `${durationminutes}:${durationseconds}`
+        }
+
+        const currentminutes = Math.floor(currentTime / 60);
+        let currentseconds = Math.floor(currentTime % 60);
+        if(currentseconds < 10){
+            currentseconds = `0${currentseconds}`;
+        }
+        if(currentseconds){
+            currenttimespan.textContent = `${currentminutes}:${currentseconds}`
+        } 
+    }
+}
+
+function setprogressbar(e){
+    // console.log(e);
+    const  width = e.srcElement.clientWidth;
+    // console.log(width);
+
+    const {duration} = music;
+
+    const clickx = e.offsetX;
+    music.currentTime = (clickx / width) * duration;
+}
+
+
+
 
 function prevsong(){
     songindex--;
@@ -69,7 +110,13 @@ function nextsong(){
     }
     loadsong(songs[songindex]);
     playsong();
-
 }
+
+prevbutton.addEventListener('click', prevsong);
+nextbutton.addEventListener('click', nextsong);
+music.addEventListener('timeupdate', updateprogressbar);
+progressDiv.addEventListener('click', setprogressbar);
+music.addEventListener('ended', nextsong);
+
 
 //Play butonuna basıldığında durdur butonu ile değişmesi ve müsic next prev butonlarının çalışması yapıldı.
