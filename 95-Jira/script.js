c794ca5cca208628361fa7ae4fb553c670ed0097
OzanMertKaraca <ozanmertkaraca@gmail.com>
2023-11-16 22:19:47 +0300

    Jira-Finish

--- 95-Jira/script.js
+++ 95-Jira/script.js
@@ -1,81 +1,27 @@
-// const todolist = document.getElementById('todolist');
-// const progresslist = document.getElementById('progresslist');
-// const donelist = document.getElementById('donelist');
-
-
-// let todolistarray = [];
-// let progresslistarray = [];
-// let donelistarray = [];
-
-// let listArrays=[];
-
-// let updatedOnload = false;
-
-// function getsavedcolumns(){
-//    if(localStorage.getItem('todoItems')){
-//          todolistarray = JSON.parse(localStorage.getItem('todoItems'));
-//          progresslistarray = JSON.parse(localStorage.getItem('progressItems'));
-//          donelistarray = JSON.parse(localStorage.getItem('doneItems'));
-//    } else{
-//       todolistarray = ['React Entegrasyonu' , 'Angular Entegrasyonu'];
-//       progresslistarray = ['Sendgrid Entegrasyonu'];
-//       donelistarray =  ['Verimor Entegrasoyunu'];
-//    }     
-// }
-
-// function updatesavedcolumn(){
-//  listArrays = [todolistarray , progresslistarray , donelistarray];
-//  const arraysname = ['todo' , 'progress' , 'done'];
-//  arraysname.forEach((arrayName , index) =>{
-//     localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
-//  })
-// }
-
-// function createItem(columnItem , column , item , index ){
-//    const listItem = document.createElement('li');
-//    listItem.classList.add('dragitem');
-//    listItem.textContent = item;
-//    columnItem.appendChild(listItem);
-
-// }
-
-
-// function updateDOM (){
-
-//    if(!updatedOnload){
-//       getsavedcolumns();
-//    }
-
-//    todolist.textContent = '';
-//    todolistarray.forEach((todoItem,index) => {
-//       createItem(todolist , 0 , todoItem , index);
-//    })
-//    progresslist.textContent = '';
-//    donelist.textContent = '';
-
-//    updatedOnload = true;
-   
-//    updatesavedcolumn();
-// }
-
-
-// updateDOM();
-
-const listcolumns = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 
 const todoList = document.getElementById('todo-list');
 const progressList = document.getElementById('progress-list');
 const doneList = document.getElementById('done-list');
+
+
+const addbuttons = document.querySelectorAll('.add-btn:not(.update)');//classı add-btn olan elemanları çağır update olanları değil.
+const savebuttons = document.querySelectorAll('.update');
+const addItemcontainers = document.querySelectorAll('.add-container');
+const addItems = document.querySelectorAll('.add-item');
+
 
 let todoListArray = [];
 let progressListArray = [];
 let doneListArray = [];
 let listArrays = [];
 
-let dragItem;
-let currentcolumn;
+let draggedItem;
+let currentColumn;
 
 let updatedOnLoad = false;
+
+let dragging = false;
 
 function getSavedColumns() {
   if (localStorage.getItem('todoItems')) {
@@ -87,26 +33,6 @@ function getSavedColumns() {
     progressListArray = ['Sendgrid Entegrasyonu'];
     doneListArray = ['Verimor Entegrasyonu'];
   }
-}
-
-function dragEnter(column){
-  currentcolumn = column;
-}
-
-function drag(e){
-    dragItem = e.target;
-    console.log(dragItem);
-}
-
-function allowDrop(e){
-    e.preventDefault();
-}
-
-function drop(e){
-  e.preventDefault();
-  const parent = listcolumns[currentcolumn];
-
-  parent.appendChild(dragItem);
 }
 
 function updateSavedColumns() {
@@ -121,17 +47,89 @@ function updateSavedColumns() {
 }
 
 function createItem(columnItem, column, item, index) {
-  // console.log('columnItem', columnItem);
-  // console.log('column', column);
-  // console.log('item', item);
-  // console.log('index', index);
+  //   console.log('columnItem', columnItem);
+  //   console.log('column', column);
+  //   console.log('item', item);
+  //   console.log('index', index);
 
   const listItem = document.createElement('li');
   listItem.classList.add('drag-item');
   listItem.textContent = item;
   listItem.draggable = true;
-  listItem.setAttribute('ondragstart' , 'drag(event)');
+  listItem.contentEditable = true;
+  listItem.setAttribute('onfocusout' , `updateItem(${index},${column})`);
+  listItem.setAttribute('ondragstart', 'drag(event)');
   columnItem.appendChild(listItem);
+}
+
+ function updateItem(id,column){
+  const selectedarray = listArrays[column];
+  const selectedColumn = listColumns[column].children;
+  console.log(selectedarray);
+ 
+
+  if (dragging === false)//!dragging 
+  {
+    if(!selectedColumn[id].textContent){
+      delete selectedarray[id];
+    }else {
+      selectedarray[id] = selectedColumn[id].textContent;
+    }
+    updateDOM();
+  } 
+ 
+
+  }
+
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+function dragEnter(column) {
+  //   console.log(listColumns[column]);
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+function updateInsideArrays() {
+  todoListArray = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    todoListArray.push(todoList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i = 0; i < progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  doneListArray = [];
+  for (let i = 0; i < doneList.children.length; i++) {
+    doneListArray.push(doneList.children[i].textContent);
+  }
+
+  updateDOM();
+}
+
+function drop(e) {
+  e.preventDefault();
+  const parent = listColumns[currentColumn];
+
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  parent.appendChild(draggedItem);
+  updateInsideArrays();
+  dragging = false;
+}
+
+function drag(e) {
+  draggedItem = e.target;
+  console.log(draggedItem);
+  dragging = true;
+}
+
+function filterarray(array){
+  const filteredarray =  array.filter((item) => item !== null);
+  return filteredarray;
+
 }
 
 function updateDOM() {
@@ -143,19 +141,46 @@ function updateDOM() {
   todoListArray.forEach((todoItem, index) => {
     createItem(todoList, 0, todoItem, index);
   });
+  todoListArray = filterarray(todoListArray);
 
   progressList.textContent = '';
   progressListArray.forEach((progressItem, index) => {
     createItem(progressList, 1, progressItem, index);
   });
+  progressListArray = filterarray(progressListArray);
 
   doneList.textContent = '';
   doneListArray.forEach((doneItem, index) => {
     createItem(doneList, 2, doneItem, index);
   });
+  doneListArray = filterarray(doneListArray);
 
   updatedOnLoad = true;
   updateSavedColumns();
+}
+
+function showitemdiv(column){
+   addbuttons[column].style.visibility = 'hidden'; 
+   addItemcontainers[column].style.display = 'flex';
+   savebuttons[column].style.display = 'flex';
+
+}
+
+function hideitemdiv(column){
+  addbuttons[column].style.visibility = 'visible'; 
+  addItemcontainers[column].style.display = 'none';
+  savebuttons[column].style.display = 'none';
+  addtocolumn(column);
+}
+
+function addtocolumn(column){
+  // console.log( addItems[column].textContent);
+  const Itemtext = addItems[column].textContent;
+   const selectedarray = listArrays[column];
+   selectedarray.push(Itemtext);
+   addItems[column].textContent = '';
+   updateDOM();
+  
 }
 
 updateDOM();
